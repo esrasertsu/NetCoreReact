390eae2c7fa30e88516e68e071e4cb4242651cd6
esrasertsu <esrasertsu@gmail.com>
2019-10-29 23:57:22 +1100

    Add project files.

---
+++ CleanArchitecture.Domain/Value.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Domain
+{
+    public class Value
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ CleanArchitecture.Persistence/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Persistence
+{
+    //as I use persistent db, I need to extend my DataContext from Entity Framework DbContext. So I add Entitiy Framework npm
+    public class DataContext: DbContext 
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+                .HasData(
+                   new Value { Id = 1, Name = "Value 101" },
+                   new Value { Id = 2, Name = "value 102" }
+
+                );
+        }
+    }
+}
