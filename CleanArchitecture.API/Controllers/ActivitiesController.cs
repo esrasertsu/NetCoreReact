3eeb2681fe9be09bcaa49614bb421f8d8611842c
esrasertsu <esrasertsu@gmail.com>
2019-11-04 00:37:21 +1100

    Create Update methods

--- CleanArchitecture.API/Controllers/ActivitiesController.cs
+++ CleanArchitecture.API/Controllers/ActivitiesController.cs
@@ -30,6 +30,17 @@ namespace CleanArchitecture.API.Controllers
         {
             return await _mediator.Send(new Details.Query { Id = id});
         }
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create(Create.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Update(Guid Id,Update.Command command)
+        {
+            command.Id = Id;
+            return await _mediator.Send(command);
+        }
 
     }
 }
