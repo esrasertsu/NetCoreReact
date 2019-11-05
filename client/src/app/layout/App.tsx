e81b5ed3bcec4edcace8e480e1a2c251481c07ca
esrasertsu <esrasertsu@gmail.com>
2019-11-05 11:44:55 +1100

    List data - React side with React hook

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,35 @@
+import React, { useState , useEffect} from 'react';
+import { Header, Icon, List } from 'semantic-ui-react';
+import axios from 'axios';
+import { IActivity } from '../models/activity';
+
+const App = () => {
+    const [activities, setActivities] = useState<IActivity[]>([]);
+    useEffect(() => {
+        axios.get<IActivity[]>('http://localhost:5000/api/activities')
+            .then((response) => {
+                setActivities(response.data)
+            })
+    },[]); //[] provides the same functionality with componentDidMounth.. 
+
+
+        return (
+            <div className="App">
+                <Header as='h2'>
+                    <Icon name='users' />
+                    <Header.Content>Reactivities</Header.Content>
+                </Header>
+                <List>
+                        {activities.map((activity) => (
+                            <List.Item key={activity.id}>{activity.title}</List.Item>
+                        ))}
+                </List>
+                   
+               
+            </div>
+        );
+    
+  
+}
+
+export default App;
